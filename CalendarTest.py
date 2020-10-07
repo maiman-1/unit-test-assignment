415f21ef1393153f9f8c53c17d7bbc2d8bb93228
Daiki <dkub0001@student.monash.edu>
2020-10-07 21:07:39 +0800

    Testing for cancel_event functionality
    I have found out that the status of the sample event is not changed to cancelled. Its status is unchanged, which means that the function itself is not working correctly I assume.

--- CalendarTest.py
+++ CalendarTest.py
@@ -50,6 +50,28 @@ class CalendarTest(unittest.TestCase):
         for event in updated_events:
             self.assertNotEqual(deleted_Event, event['id'])
 
+    def test_cancel_events(self):
+        """
+        This test cancels an event and check if its status is cancelled.
+        """
+        mock_api = Calendar.get_calendar_api()
+        time_now = datetime.datetime.utcnow()
+        option = 2
+
+        events = Calendar.get_all_events(mock_api, time_now)
+
+        canceled_event = events[option - 1]
+        canceled_event_id = events[option - 1]['id']
+        Calendar.cancel_event(mock_api, canceled_event_id)
+
+        """
+        After the cancellation of an event, obtain the updated calendar.
+        Tests if Cancelled Event is still on the calendar
+        """
+        self.assertEqual(
+            'cancelled', canceled_event['status'])  # It prints out confirmed.. I think there is something wrong with
+        # the functionality
+        # AssertionError: 'cancelled' != 'confirmed'
 
 def main():
     # Create the test suite from the cases above.
